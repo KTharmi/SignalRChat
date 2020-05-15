67acb9c4f77946cda84d8e63e51b93e173dc0868
KTharmi <tharmikulendiran5@gmail.com>
2020-05-15 06:21:56 -0800

    SignalR concept

---
+++ Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// Note: 2
+using Owin;
+using Microsoft.Owin;
+//Note :3
+[assembly: OwinStartup(typeof(SignalRChat.Startup))]
+
+namespace SignalRChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Note: 4
+            app.MapSignalR();
+        }
+    }
+}
